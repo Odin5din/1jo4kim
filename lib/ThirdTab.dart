e962f38bac15a0213e0a78ede45894fb636d3956
Odin5din <133902344+Odin5din@users.noreply.github.com>
2023-07-11 22:06:43 +0900

    오버플로우,CRUD 해결

--- lib/ThirdTab.dart
+++ lib/ThirdTab.dart
@@ -23,106 +23,125 @@ class _ThirdTabState extends State<ThirdTab> {
       builder: (context, memoService, child) {
         List<Memo> memoList = memoService.memoList;
 
-        return Scaffold(
-          backgroundColor: Colors.white,
-          body: Padding(
-            padding: const EdgeInsets.all(20),
-            child: Column(
-              children: [
-                // 이미지 경로 설정
-                Text(
-                  '김지견',
-                  style: TextStyle(
-                    color: Colors.black,
-                  ),
-                ),
-                Row(
+        return Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Scaffold(
+            backgroundColor: Colors.white,
+            body: Padding(
+              padding: const EdgeInsets.all(20),
+              child: SingleChildScrollView(
+                child: Column(
                   children: [
-                    Text("수정"),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 24),
-                        child: TextField(),
+                    // 이미지 경로 설정
+                    Text(
+                      '김지견',
+                      style: TextStyle(
+                        color: Colors.black,
                       ),
                     ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("강점"),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 24),
-                        child: TextField(),
-                      ),
+                    Row(
+                      children: [
+                        Text("수정"),
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 24),
+                            child: TextField(),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("강점"),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 24),
-                        child: TextField(),
-                      ),
+                    Row(
+                      children: [
+                        Text("강점"),
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 24),
+                            child: TextField(),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Row(
-                  children: [
-                    Text("강점"),
-                    Expanded(
-                      child: Container(
-                        margin: const EdgeInsets.only(left: 24),
-                        child: TextField(),
-                      ),
+                    Row(
+                      children: [
+                        Text("강점"),
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 24),
+                            child: TextField(),
+                          ),
+                        ),
+                      ],
                     ),
-                  ],
-                ),
-                Spacer(),
-                memoList.isEmpty
-                    ? Center(
-                        child: Text("입력된 메모가 없습니다."),
-                      )
-                    : ListView.builder(
-                        itemCount: memoList.length,
-                        itemBuilder: (context, index) {
-                          Memo memo = memoList[index];
-                          return ListTile(
-                            title: Text(
-                              memo.content,
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                            ),
-                            onTap: () {
-                              Navigator.push(
-                                context,
-                                MaterialPageRoute(
-                                  builder: (_) => DetailPage(
-                                    index: index,
-                                  ),
+                    Row(
+                      children: [
+                        Text("강점"),
+                        Expanded(
+                          child: Container(
+                            margin: const EdgeInsets.only(left: 24),
+                            child: TextField(),
+                          ),
+                        ),
+                      ],
+                    ),
+
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: memoList.isEmpty
+                          ? Center(
+                              child: Text("입력된 메모가 없습니다."),
+                            )
+                          : Column(
+                              children: [
+                                ListView.builder(
+                                  shrinkWrap: true,
+                                  itemCount: memoList.length,
+                                  itemBuilder: (context, index) {
+                                    Memo memo = memoList[index];
+                                    return Padding(
+                                      padding: const EdgeInsets.all(8.0),
+                                      child: ListTile(
+                                        title: Text(
+                                          memo.content,
+                                          maxLines: 3,
+                                          overflow: TextOverflow.ellipsis,
+                                          style: TextStyle(
+                                            color: Colors.black,
+                                          ),
+                                        ),
+                                        onTap: () {
+                                          Navigator.push(
+                                            context,
+                                            MaterialPageRoute(
+                                              builder: (_) => DetailPage(
+                                                index: index,
+                                              ),
+                                            ),
+                                          );
+                                        },
+                                      ),
+                                    );
+                                  },
                                 ),
-                              );
-                            },
-                          );
-                        },
-                      ),
-              ],
-            ),
-          ),
-          floatingActionButton: FloatingActionButton(
-            child: Icon(Icons.edit),
-            onPressed: () {
-              memoService.createMemo(content: '');
-              Navigator.push(
-                context,
-                MaterialPageRoute(
-                  builder: (_) =>
-                      DetailPage(index: memoService.memoList.length - 1),
+                              ],
+                            ),
+                    ),
+                  ],
                 ),
-              );
-            },
+              ),
+            ),
+            floatingActionButton: FloatingActionButton(
+              child: Icon(Icons.edit),
+              onPressed: () {
+                memoService.createMemo(content: '');
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (_) =>
+                        DetailPage(index: memoService.memoList.length - 1),
+                  ),
+                );
+              },
+            ),
           ),
         );
       },
