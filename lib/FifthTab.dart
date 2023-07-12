10a0663786680ff1020d7a686d4e535f8d6b2c15
Odin5din <133902344+Odin5din@users.noreply.github.com>
2023-07-12 12:04:57 +0900

    asdads

--- lib/FifthTab.dart
+++ lib/FifthTab.dart
@@ -1,7 +1,8 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
-import 'memo_service.dart';
+import 'main.dart';
 
 class FifthTab extends StatefulWidget {
   const FifthTab({Key? key}) : super(key: key);
@@ -13,9 +14,9 @@ class FifthTab extends StatefulWidget {
 class _FifthTabState extends State<FifthTab> {
   @override
   Widget build(BuildContext context) {
-    return Consumer<MemoService>(
-      builder: (context, memoService, child) {
-        List<Memo> memoList = memoService.memoList;
+    return Consumer<Memo5Service>(
+      builder: (context, memo5Service, child) {
+        List<Memo5> memo5List = memo5Service.memo5List;
 
         return Padding(
           padding: const EdgeInsets.all(8.0),
@@ -80,7 +81,7 @@ class _FifthTabState extends State<FifthTab> {
 
                     Padding(
                       padding: const EdgeInsets.all(8.0),
-                      child: memoList.isEmpty
+                      child: memo5List.isEmpty
                           ? Center(
                               child: Text("입력된 메모가 없습니다."),
                             )
@@ -88,14 +89,14 @@ class _FifthTabState extends State<FifthTab> {
                               children: [
                                 ListView.builder(
                                   shrinkWrap: true,
-                                  itemCount: memoList.length,
+                                  itemCount: memo5List.length,
                                   itemBuilder: (context, index) {
-                                    Memo memo = memoList[index];
+                                    Memo5 memo5 = memo5List[index];
                                     return Padding(
                                       padding: const EdgeInsets.all(8.0),
                                       child: ListTile(
                                         title: Text(
-                                          memo.content,
+                                          memo5.content,
                                           maxLines: 3,
                                           overflow: TextOverflow.ellipsis,
                                           style: TextStyle(
@@ -126,12 +127,12 @@ class _FifthTabState extends State<FifthTab> {
             floatingActionButton: FloatingActionButton(
               child: Icon(Icons.edit),
               onPressed: () {
-                memoService.createMemo(content: '');
+                memo5Service.createMemo5(content: '');
                 Navigator.push(
                   context,
                   MaterialPageRoute(
                     builder: (_) =>
-                        DetailPage(index: memoService.memoList.length - 1),
+                        DetailPage(index: memo5Service.memo5List.length - 1),
                   ),
                 );
               },
@@ -140,5 +141,143 @@ class _FifthTabState extends State<FifthTab> {
         );
       },
     );
+  }
+}
+
+class DetailPage extends StatelessWidget {
+  DetailPage({super.key, required this.index});
+
+  final int index;
+
+  TextEditingController contentController = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    Memo5Service memo5Service = context.read<Memo5Service>();
+    Memo5 memo5 = memo5Service.memo5List[index];
+
+    contentController.text = memo5.content;
+
+    return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            onPressed: () {
+              showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text("정말로 삭제하시겠습니까?"),
+                    actions: [
+                      TextButton(
+                        onPressed: () {
+                          Navigator.pop(context);
+                        },
+                        child: Text("취소"),
+                      ),
+                      TextButton(
+                        onPressed: () {
+                          memo5Service.deleteMemo5(index: index);
+                          Navigator.pop(context);
+                          Navigator.pop(context);
+                        },
+                        child: Text(
+                          "확인",
+                          style: TextStyle(color: Colors.pink),
+                        ),
+                      ),
+                    ],
+                  );
+                },
+              );
+            },
+            icon: Icon(Icons.delete),
+          )
+        ],
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16),
+        child: TextField(
+          controller: contentController,
+          decoration: InputDecoration(
+            hintText: "메모를 입력하세요",
+            border: InputBorder.none,
+          ),
+          autofocus: true,
+          maxLines: null,
+          expands: true,
+          keyboardType: TextInputType.multiline,
+          onChanged: (value) {
+            memo5Service.updateMemo5(index: index, content: value);
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class Memo5 {
+  Memo5({
+    required this.content,
+  });
+
+  String content;
+
+  Map toJson() {
+    return {'content': content};
+  }
+
+  factory Memo5.fromJson(json) {
+    return Memo5(content: json['content']);
+  }
+}
+
+class Memo5Service extends ChangeNotifier {
+  Memo5Service() {
+    loadMemo5List();
+  }
+
+  List<Memo5> memo5List = [];
+
+  createMemo5({required String content}) {
+    Memo5 memo5 = Memo5(content: content);
+    memo5List.add(memo5);
+    notifyListeners();
+    saveMemo5List(); // Consumer<Memo5Service>의 builder 부분을 호출해서 화면 새로고침
+  }
+
+  updateMemo5({required int index, required String content}) {
+    Memo5 memo5 = memo5List[index];
+    memo5.content = content;
+    notifyListeners();
+    saveMemo5List();
+  }
+
+  deleteMemo5({required int index}) {
+    memo5List.removeAt(index);
+    notifyListeners();
+    saveMemo5List();
+  }
+
+  saveMemo5List() {
+    List memo5JsonList = memo5List.map((memo5) => memo5.toJson()).toList();
+    // [{"content": "1"}, {"content": "2"}]
+
+    String jsonString = jsonEncode(memo5JsonList);
+    // '[{"content": "1"}, {"content": "2"}]'
+
+    prefs.setString('memo5List', jsonString);
+  }
+
+  loadMemo5List() {
+    String? jsonString = prefs.getString('memo5List');
+    // '[{"content": "1"}, {"content": "2"}]'
+
+    if (jsonString == null) return; // null 이면 로드하지 않음
+
+    List memo5JsonList = jsonDecode(jsonString);
+    // [{"content": "1"}, {"content": "2"}]
+
+    memo5List = memo5JsonList.map((json) => Memo5.fromJson(json)).toList();
   }
 }
